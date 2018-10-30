fdfa91125fd2a4b9b44f01476f7f734c21184e77
Mapitso15 <mamiptso.com>
2018-10-30 15:26:30 +0200

    adding something

---
+++ my-project/upload.js
@@ -0,0 +1,27 @@
+const http = require('http');
+let formidable = require('formidable');
+let fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    let form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      let oldpath = files.filetoupload.path;
+      let newpath = 'my-project' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('<img src = "my-project/c1.jpeg">');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+
+
+}).listen(8080);
